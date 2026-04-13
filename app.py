1dab7ff00063b2cd08d3f1d63d112c6eb9472821
miskatsikder-boop <miskatsikder@icloud.com>
2026-04-13 20:07:08 +0100

    Update Flask app initialization with static folder

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import requests
 import random
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.secret_key = "63f4945d921d599f27ae4fdf5bada3f1"
 
 saved_players = []
